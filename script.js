37412909abcd68482b79028ba9178fd6dd0d766f
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-11-26 09:16:58 -0500

    Added prompts for the user to specify length and use of character types in password. Using an alert to require length >7 and <128

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = prompt("How many characters would you like to use?");
+  if (length < 8 || length > 128) {
+    alert("Length must be at least 8 characters and no more than 128 characters!");
+    var length = prompt("How many characters would you like to use?");
+  }
+
+var upperCase = confirm("Would you like to use UPPERCASE letters?");
+var lowerCase = confirm("Would you like to use lowercaser letters?");
+var numbers = confirm("Would you like to use numbers?")
+var symbols = confirm("Would you like to use special characters?")
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
