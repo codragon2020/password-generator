72eb983575f582e4348ac50af14dfa276bf66e92
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-11-27 06:28:46 -0500

    Updated the confirm language to be more specific. Fix logic for >=8 && <=128 length.

--- script.js
+++ script.js
@@ -2,42 +2,49 @@
 var generateBtn = document.querySelector("#generate");
 
 var length = prompt("How many characters would you like to use?");
-  if (length < 8 || length > 128) {
+  while (length < 8 || length > 128) {
     alert("Length must be at least 8 characters and no more than 128 characters!");
     var length = prompt("How many characters would you like to use?");
   }
 
-var upperCase = confirm("Would you like to use UPPERCASE letters?");
-var lowerCase = confirm("Would you like to use lowercaser letters?");
-var numbers = confirm("Would you like to use numbers?")
-var symbols = confirm("Would you like to use special characters?")
+var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
+var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
+var numbers = confirm("Click OK to confirm you would like to use numbers?")
+var symbols = confirm("Click OK to confirm you would like to use special characters?")
 
-//storing the values of user inputs into this array
-var array = [upperCase, lowerCase, numbers, symbols];
-console.log(array);
+// Arrays to be used
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+
+
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var num = '0123456789';
-var sym = '!@#$%^&*=-_';
-
 function writePassword() {
-  var retVal = "";
-  for (var i = 0, n = char.length; i < length; ++i) {
-      retVal += char.charAt(Math.floor(Math.random() * n));
-      console.log(retVal);
-  }
-  ;
-  return retVal;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
+console.log(writePassword(12,char));
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// function writePassword(l,characters) {
+//   var pwd = "";
+  
+//   for (var i = 0; i < l; i++) {
+//       pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+//   };
+  
+//   return pwd;
+// };
+
+
+
