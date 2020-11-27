5178d2f5c6bb7ee74112c69635685ec9af6c3c9f
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-11-27 06:39:23 -0500

    Added While loop to ensure at least one parameter is set

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ var length = prompt("How many characters would you like to use?");
     var length = prompt("How many characters would you like to use?");
   }
 
-var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
-var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
-var numbers = confirm("Click OK to confirm you would like to use numbers?")
-var symbols = confirm("Click OK to confirm you would like to use special characters?")
+    var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
+    var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
+    var numbers = confirm("Click OK to confirm you would like to use numbers?");
+    var symbols = confirm("Click OK to confirm you would like to use special characters?");
 
+  while(upperCase === false && lowerCase === false && symbols === false && numbers === false) {
+    alert("You must choose at least one parameter");
+    var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
+    var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
+    var numbers = confirm("Click OK to confirm you would like to use numbers?");
+    var symbols = confirm("Click OK to confirm you would like to use special characters?");
+  };
 // Arrays to be used
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -45,6 +52,3 @@ generateBtn.addEventListener("click", writePassword);
   
 //   return pwd;
 // };
-
-
-
