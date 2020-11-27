9124d8a310a9bdd109fa057f706c07e332152a53
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-11-27 06:49:08 -0500

    Created empty array for password to be constructed

--- script.js
+++ script.js
@@ -19,14 +19,16 @@ var length = prompt("How many characters would you like to use?");
     var numbers = confirm("Click OK to confirm you would like to use numbers?");
     var symbols = confirm("Click OK to confirm you would like to use special characters?");
   };
+
 // Arrays to be used
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
+var passwordCharacters = []
 
-
+console.log(passwordCharacters);
 
 // Write password to the #password input
 function writePassword() {
@@ -37,18 +39,5 @@ function writePassword() {
 
 }
 
-console.log(writePassword(12,char));
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// function writePassword(l,characters) {
-//   var pwd = "";
-  
-//   for (var i = 0; i < l; i++) {
-//       pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-//   };
-  
-//   return pwd;
-// };
