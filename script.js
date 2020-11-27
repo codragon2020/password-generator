6f06920cb7b07bd4c2b94d8901aae854123d9f05
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-11-27 07:20:13 -0500

    Testing concat method to see how it works with arrays

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var length = prompt("How many characters would you like to use?");
-  while (length < 8 || length > 128) {
-    alert("Length must be at least 8 characters and no more than 128 characters!");
-    var length = prompt("How many characters would you like to use?");
+// Function to Generate Password when button is clicked
+
+  var confirmLength = prompt("How many characters would you like to use?");
+    while (confirmLength < 8 || confirmLength > 128) {
+      alert("Length must be at least 8 characters and no more than 128 characters!");
+      var confirmLength = prompt("How many characters would you like to use?");
+    }
+
+      var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
+      var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
+      var numbers = confirm("Click OK to confirm you would like to use numbers?");
+      var symbols = confirm("Click OK to confirm you would like to use special characters?");
+
+    while(upperCase === false && lowerCase === false && symbols === false && numbers === false) {
+      alert("You must choose at least one parameter");
+      var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
+      var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
+      var numbers = confirm("Click OK to confirm you would like to use numbers?");
+      var symbols = confirm("Click OK to confirm you would like to use special characters?");
+    };
+
+  // Arrays to be used
+  var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+  var pwdChars = []
+
+  if (upperCase) {
+    pwdChars = pwdChars.concat(alphaUpper)
   }
 
-    var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
-    var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
-    var numbers = confirm("Click OK to confirm you would like to use numbers?");
-    var symbols = confirm("Click OK to confirm you would like to use special characters?");
+  if (lowerCase) {
+    pwdChars = pwdChars.concat(alphaLower)
+  }
 
-  while(upperCase === false && lowerCase === false && symbols === false && numbers === false) {
-    alert("You must choose at least one parameter");
-    var upperCase = confirm("Click OK to confirm you would like to use UPPERCASE letters?");
-    var lowerCase = confirm("Click OK to confirm you would like to use lowercase letters?");
-    var numbers = confirm("Click OK to confirm you would like to use numbers?");
-    var symbols = confirm("Click OK to confirm you would like to use special characters?");
-  };
 
-// Arrays to be used
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+  console.log(pwdChars);
 
-var passwordCharacters = []
-
-console.log(passwordCharacters);
 
 // Write password to the #password input
 function writePassword() {
